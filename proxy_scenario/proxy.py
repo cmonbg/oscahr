e244697340a07c53c165228e83bebfbd5a334877
RainerPum <s1910304014@students.fh-hagenberg.at>
2022-05-15 21:11:44 +0200

    fix strange symbol in the beginning of the received command, second attempt, debugging

--- proxy_scenario/proxy.py
+++ proxy_scenario/proxy.py
@@ -260,7 +260,7 @@ class Proxy:
 
             # for some strange reason there is a singular '\' or '[' in the beginning, removen
             # it
-            command = command[1:]
+            command = command[5:]
 
             self._log.info(f"Processing command '{command}' with parameter '{parameter}'")
 
